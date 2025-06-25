a312d2bbd247e459f0a94d735c47bf9ef046c8f9
Arthur Bräul <a.braeul@live.de>
2025-06-25 17:13:39 +0200

    edit-components created

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -6,6 +6,10 @@ import { User } from '../../models/user.class';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogEditAddressComponent } from '../dialog-edit-address/dialog-edit-address.component';
+import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
+
 
 @Component({
   selector: 'app-user-detail',
@@ -22,7 +26,7 @@ export class UserDetailComponent {
   user: User = new User();
   error: string | null = null;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(paramMap => {
@@ -82,7 +86,11 @@ export class UserDetailComponent {
     });
   }
 
-  editAdress() { }
+  editAdress() {
+    this.dialog.open(DialogEditAddressComponent);
+  }
 
-  editUserDetail() { }
+  editUserDetail() {
+    this.dialog.open(DialogEditUserComponent);
+  }
 }
